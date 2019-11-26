ab650ab50db1d05554d8f8062ff85a88c97e4319
EShau <EShau00@stuy.edu>
2019-11-26 18:34:01 -0500

    gfeg

--- parse_main.c
+++ parse_main.c
@@ -6,10 +6,11 @@
 char ** parse_args( char * line );
 
 char ** parse_args( char * line ){
-  char ** args = malloc(6);
+  char ** args = calloc(6, sizeof(char));
   char line_array[100];
   strcpy(line_array, line);
   char * line_parse = line_array;
+  printf("%s\n", line_array);
   int i = 0;
   while (line_parse){
     char temp[100];
@@ -17,6 +18,11 @@ char ** parse_args( char * line ){
     strcpy(temp, token);
     args[i] = malloc(sizeof(temp));
     strcpy(args[i], temp);
+    printf("%s\n", args[i]);
+    i++;
+  }
+  while (i < 6){
+    args[i] = NULL;
     i++;
   }
   return args;
@@ -28,12 +34,15 @@ int main(){
   printf("Running parse_args on the invocation...\n");
   char ** args = parse_args(test_string);
   printf("ok\n");
-  // char * args[6];
   int i = 0;
   printf("ok\n");
-  // printf("%s\n", *(args_temp));
+   for (i = 0; i < 6; i++){
+     printf("%s\n", *(args + i));
+   }
+  printf("ok\n");
+  char * y[] = {"ls", "-a", "-l", NULL};
   printf("%s\n", args[0]);
   printf("Running execvp for the invocation...\n");
-  execvp(args[0], args);
+  execvp(args[0], y);
   return 0;
 }
