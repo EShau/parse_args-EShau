3e5501a6d120e12c64e34b6c6fd17b60844cb983
EShau <EShau00@stuy.edu>
2019-11-26 19:49:44 -0500

    DONE

--- parse_main.c
+++ parse_main.c
@@ -6,16 +6,17 @@
 char ** parse_args( char * line );
 
 char ** parse_args( char * line ){
-  char ** args = malloc(1);
+  char ** args = malloc(6 * sizeof(char *));
   char line_array[100];
   strcpy(line_array, line);
   char * line_parse = line_array;
   int i = 0;
   while (line_parse){
-    args[i] = strsep(&line_parse, " ");
-    printf("%s\n", args[i]);
+    char temp[50];
+    strcpy(temp, strsep(&line_parse, " "));
+    args[i] = malloc(strlen(temp) + 1);
+    strcpy(args[i], temp);
     i++;
-    args = realloc(args, i);
   }
   args[i] = NULL;
   return args;
@@ -26,11 +27,6 @@ int main(){
   printf("\"Command line invocation\": %s\n", test_string);
   printf("Running parse_args on the invocation...\n");
   char ** args = parse_args(test_string);
-  int i = 0;
-  while (args[i]){
-    printf("%s\n", args[i]);
-    i++;
-  }
   printf("Running execvp for the invocation...\n");
   execvp(args[0], args);
   return 0;
