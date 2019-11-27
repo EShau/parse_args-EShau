e81f053c775d76130b0c77dd1b84b87ffc5ee816
EShau <EShau00@stuy.edu>
2019-11-26 19:00:21 -0500

    gorenv

--- parse_main.c
+++ parse_main.c
@@ -6,27 +6,17 @@
 char ** parse_args( char * line );
 
 char ** parse_args( char * line ){
-  char ** args = calloc(6, sizeof(char));
+  char ** args = calloc(8, sizeof(char *));
   char line_array[100];
   strcpy(line_array, line);
   char * line_parse = line_array;
-  printf("%s\n", line_array);
   int i = 0;
   while (line_parse){
-    char temp[100];
-    char * token = strsep(&line_parse, " ");
-    strcpy(temp, token);
-    args[i] = malloc(sizeof(temp));
-    strcpy(args[i], temp);
-    printf("Argument: %s\n", args[i]);
+    args[i] = strsep(&line_parse, " ");
+    printf("%s\n", args[i]);
     i++;
   }
-  printf("New iteration %s\n", args[4]);
-  i = 0;
-  while (i < 6){
-    i++;
-    printf("Argument: %s\n", args[i]);
-  }
+  args[i] = NULL;
   return args;
 }
 
@@ -35,16 +25,12 @@ int main(){
   printf("\"Command line invocation\": %s\n", test_string);
   printf("Running parse_args on the invocation...\n");
   char ** args = parse_args(test_string);
-  printf("ok\n");
   int i = 0;
-  printf("ok\n");
-  for (i = 0; i < 6; i++){
-    printf("%s\n", *(args + i));
+  while (args[i]){
+    printf("%s\n", args[i]);
+    i++;
   }
-  printf("ok\n");
-  char * y[] = {"ls", "-a", "-l", NULL};
-  printf("%s\n", args[0]);
   printf("Running execvp for the invocation...\n");
-  execvp(args[0], y);
+  execvp(args[0], args);
   return 0;
 }
